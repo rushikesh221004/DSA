f5665f5c0e2a7bec1e0452abd2dacf6c6b938b5f
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2026-08-28 10:36:48 +0530

    Created: findMin function created

--- Binary Search/Hard/FindMinimumInRotatedSortedArrayII.java
+++ Binary Search/Hard/FindMinimumInRotatedSortedArrayII.java
@@ -1,4 +1,9 @@
 class FindMinimumInRotatedSortedArrayII {
+
+    public static int findMin(int[] nums) {
+        
+    }
+
     int[] nums = {2, 2, 2, 0, 1};
 
     System.out.println(findMin(nums));
