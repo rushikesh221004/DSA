4b387980dea990a0711268ea912d9c873f3cf0cd
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2026-08-30 18:53:00 +0530

    Initialized: matrix variables initialized

--- Binary Search/Medium/SearchA2DMatrix.java
+++ Binary Search/Medium/SearchA2DMatrix.java
@@ -1,5 +1,9 @@
 class SearchA2DMatrix {
     public static void main(String[] args) {
-        
+        int[][] matrix = {
+            {1, 3, 5, 7},
+            {10, 11, 16, 20},
+            {23, 30, 34, 60}
+        };
     }
 }
