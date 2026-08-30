5199fbed64fb27eac4017986b8d39e8b975a6cd8
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2026-08-30 18:56:30 +0530

    Created: searchMatrix() function created

--- Binary Search/Medium/SearchA2DMatrix.java
+++ Binary Search/Medium/SearchA2DMatrix.java
@@ -1,4 +1,9 @@
 class SearchA2DMatrix {
+
+    public static int searchMatrix(int[][] matrix, int target) {
+        
+    }
+
     public static void main(String[] args) {
         int[][] matrix = {
             {1, 3, 5, 7},
