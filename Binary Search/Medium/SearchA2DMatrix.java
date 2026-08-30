505b73602f2469fb1ae591edd588d0f5b3aca7b9
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2026-08-30 19:00:44 +0530

    Solved: Leetcode (74) --> Search a 2D Matrix

--- Binary Search/Medium/SearchA2DMatrix.java
+++ Binary Search/Medium/SearchA2DMatrix.java
@@ -1,14 +1,34 @@
 class SearchA2DMatrix {
 
-    public static int searchMatrix(int[][] matrix, int target) {
-        
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+
+        int start = 0, end = (m * n) - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            int row = mid / n;
+            int col = mid % n;
+
+            if (matrix[row][col] == target)
+                return true;
+
+            if (matrix[row][col] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return false;
     }
 
     public static void main(String[] args) {
         int[][] matrix = {
-            {1, 3, 5, 7},
-            {10, 11, 16, 20},
-            {23, 30, 34, 60}
+                { 1, 3, 5, 7 },
+                { 10, 11, 16, 20 },
+                { 23, 30, 34, 60 }
         };
         int target = 3;
 
