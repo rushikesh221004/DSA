517aa1d3609a8c615a7b1a83baa707836a772e91
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2026-08-31 21:53:17 +0530

    Solved: Leetcode(240) --> Search a 2D Matrix II

---
+++ Binary Search/Medium/SearchA2DMatrixII.java
@@ -0,0 +1,35 @@
+class SearchA2DMatrixII {
+
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length - 1;
+
+        int row = 0, col = n;
+
+        while (row < m && col >= 0) {
+            if (matrix[row][col] == target)
+                return true;
+
+            if (matrix[row][col] > target) {
+                col--;
+            } else {
+                row++;
+            }
+
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                { 1, 4, 7, 11, 15 },
+                { 2, 5, 8, 12, 19 },
+                { 3, 6, 9, 16, 22 },
+                { 10, 13, 14, 17, 24 },
+                { 18, 21, 23, 26, 30 }
+        };
+        int target = 5;
+
+        System.out.println(searchMatrix(matrix, target));
+    }
+}
